0a1ed8f70f18b5b5eb35d59c718b545b89bfa7fb
hackshuu <marcos.mariano7@hotmail.com>
2020-08-21 23:17:13 -0300

    Random quotes and random quantity

--- get-quote.py
+++ get-quote.py
@@ -10,8 +10,12 @@ def principal():
 
   last = len(quotes) -1
   rnd = random.randint(0,last)
-
-  print(quotes[rnd])
+  quantidade = rnd
+  count = 0;
+  while count <=quantidade:
+        rnd = random.randint(0,last)
+        print(quotes[rnd])
+        count = count+1
 
 if __name__== "__main__":
   principal()
